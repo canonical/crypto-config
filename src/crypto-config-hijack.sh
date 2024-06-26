7b69c433fe1bdb916dd5160654e7f9cec7bbdf90
Adrien Nader <adrien.nader@canonical.com>
2024-06-26 14:52:11 +0200

    Release 0.5. It's 0.4 without the bugs.

--- src/crypto-config-hijack.sh
+++ src/crypto-config-hijack.sh
@@ -3,11 +3,11 @@
 set -e
 set -u
 
-CURRENT='/var/lib/crypto-config/current'
+CURRENT='/var/lib/crypto-config/profiles/current'
 
 echo 'openssl'
-if ! grep -q crypto-config '/etc/ssl/openssl.cnf'; then
-  echo ".include ${CURRENT}/openssl.d" >> '/etc/ssl/openssl.cnf'
+if ! grep -q 'crypto-config' '/etc/ssl/openssl.cnf'; then
+  echo ".include ${CURRENT}/openssl.conf.d" >> '/etc/ssl/openssl.cnf'
 fi
 
 echo 'gnutls'
@@ -16,4 +16,4 @@ ln -sfn "${CURRENT}/gnutls.conf" '/etc/gnutls/config'
 
 echo 'nginx'
 sed -i '/ssl_protocols/ d' '/etc/nginx/nginx.conf'
-echo "include ${CURRENT}/nginx.conf.d/*.conf" > '/etc/nginx/conf.d/crypto-config.conf'
+echo "include ${CURRENT}/nginx.conf.d/*.conf;" > '/etc/nginx/conf.d/crypto-config.conf'
