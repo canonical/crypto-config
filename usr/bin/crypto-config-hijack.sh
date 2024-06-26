b48ae7351c180b2b347b0683d28abec46b23859a
Adrien Nader <adrien.nader@canonical.com>
2024-06-26 13:31:49 +0200

    tool: add crypto-config-hijack.sh

---
+++ usr/bin/crypto-config-hijack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -u
+
+CURRENT='/var/lib/crypto-config/current'
+
+echo 'openssl'
+if ! grep -q crypto-config '/etc/ssl/openssl.cnf'; then
+  echo ".include ${CURRENT}/openssl.d" >> '/etc/ssl/openssl.cnf'
+fi
+
+echo 'gnutls'
+# No dropin support for gnutls
+ln -sfn "${CURRENT}/gnutls.conf" '/etc/gnutls/config'
+
+echo 'nginx'
+sed -i '/ssl_protocols/ d' '/etc/nginx/nginx.conf'
+echo "include ${CURRENT}/nginx.conf.d/*.conf" > '/etc/nginx/conf.d/crypto-config.conf'
