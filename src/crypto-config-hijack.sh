a2b57b536effe1f355e042462708bddbf9f854d3
Adrien Nader <adrien.nader@canonical.com>
2024-06-27 15:26:59 +0200

    Version 0.5.1.

--- src/crypto-config-hijack.sh
+++ src/crypto-config-hijack.sh
@@ -11,10 +11,11 @@ if ! grep -q 'crypto-config' '/etc/ssl/openssl.cnf'; then
 fi
 
 echo 'gnutls'
-# No dropin support for gnutls
+# No drop-in support for gnutls
 ln -sfn "${CURRENT}/gnutls.conf" '/etc/gnutls/config'
 
 echo 'nginx'
+# XXX report issue below
 sed -i '/ssl_protocols/ d' '/etc/nginx/nginx.conf'
 echo "include ${CURRENT}/nginx.conf.d/*.conf;" > '/etc/nginx/conf.d/crypto-config.conf'
 
