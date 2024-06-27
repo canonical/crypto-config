4dd4c734531dc624e63ddf65a49c78e36f4285de
Adrien Nader <adrien.nader@canonical.com>
2024-06-27 12:17:40 +0200

    profiles: support apt with Key::Assert:Pubkey-Algo.

--- src/crypto-config-hijack.sh
+++ src/crypto-config-hijack.sh
@@ -11,9 +11,15 @@ if ! grep -q 'crypto-config' '/etc/ssl/openssl.cnf'; then
 fi
 
 echo 'gnutls'
-# No dropin support for gnutls
+# No drop-in support for gnutls
 ln -sfn "${CURRENT}/gnutls.conf" '/etc/gnutls/config'
 
 echo 'nginx'
+# XXX report issue below
 sed -i '/ssl_protocols/ d' '/etc/nginx/nginx.conf'
 echo "include ${CURRENT}/nginx.conf.d/*.conf;" > '/etc/nginx/conf.d/crypto-config.conf'
+
+echo 'apt'
+for k in 'assert-pubkey-algo'; do
+  ln -sfn "${CURRENT}/apt.conf.d/${k}.conf" "/etc/apt/apt.conf.d/${k}.conf"
+done
